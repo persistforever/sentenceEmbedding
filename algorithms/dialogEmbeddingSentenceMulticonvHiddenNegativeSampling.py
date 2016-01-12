cf06e5c014b158ed342daf741df55b11396fb4df
jiafeimaowudi <jiafeimaowudi@163.com>
2016-01-12 12:57:32 +0800

    multi

---
+++ algorithms/dialogEmbeddingSentenceMulticonvHiddenNegativeSampling.py
@@ -0,0 +1,122 @@
+# coding=utf-8
+# -*- coding: utf-8 -*-
+from layers.mlp import HiddenLayer
+from theano import tensor as T, printing
+import theano
+import numpy
+from  layers.sentenceEmbeddingMultiNN import SentenceEmbeddingMultiNN
+from algorithms.algorithm import algorithm
+import util
+import config
+import string
+
+class sentenceEmbeddingMulticonvHiddenNegativeSampling(algorithm):
+    
+    def __init__(self, input_params=None):
+        rng = numpy.random.RandomState(23455)
+        self._corpusWithEmbeddings = T.matrix("wordIndeices")
+        self._dialogSentenceCount = T.ivector("dialogSentenceCount")
+        self._sentenceWordCount = T.ivector("sentenceWordCount")
+        
+        # for list-type data
+        self._layer0 = layer0 = SentenceEmbeddingMultiNN(self._corpusWithEmbeddings, self._dialogSentenceCount, self._sentenceWordCount, rng, wordEmbeddingDim=200, \
+                                                         sentenceLayerNodesNum=[150, 120], \
+                                                         sentenceLayerNodesSize=[(2, 200), (3, 1)],
+                                                         poolingSize=[(2, 1)],
+                                                         mode="max")
+        
+        layer1 = HiddenLayer(
+            rng,
+            input=layer0.output,
+            n_in=layer0.outputDimension,
+            n_out=layer0.outputDimension,
+            activation=T.tanh
+        )
+        self._nextSentence = layer1.output
+        self._params = layer1.params + layer0.params
+        self._setParameters(input_params)
+    
+    def getTrainFunction(self, cr, cr_scope, batchSize=10, errorType="RMSE"):
+        normalizationError = 0
+        for p in self._params:
+            normalizationError += 0.5 / batchSize * T.sum(T.square(p))
+            
+        isAStartSentence = T.ivector("isAStartSentence")
+        iass = 1 - isAStartSentence[(self._dialogSentenceCount[0] + 1):self._dialogSentenceCount[-1]]
+        
+        availableIndex = iass.nonzero()
+        
+        error = util.getError(self._nextSentence[:-1][availableIndex], self._layer0.output[1:][availableIndex], errorType)
+        errorSum = T.sum(error)
+        
+        errorNegative = util.getError(self._nextSentence[:-1][availableIndex], self._layer0.output[-1:0:-1][availableIndex], errorType)
+        errorSumNegative = T.sum(errorNegative)
+        
+        learning_rate = 0.01
+        
+        normalizationLambda = 0.0
+        negativeLambda = 1
+#         e = errorSum - negativeLambda * errorSumNegative + normalizationLambda * normalizationError
+        e = negativeLambda * errorSum / errorSumNegative + normalizationLambda * normalizationError
+        
+        grads = T.grad(e, self._params)
+        updates = [
+            (param_i, param_i - learning_rate * grad_i)
+            for param_i, grad_i in zip(self._params, grads)
+        ]
+        print "Loading data."
+        dialogMatrixes, docSentenceNums, sentenceWordNums, sl, _ = cr.getCorpus(cr_scope, 6, onlyFront=True)
+        
+        for s in sl:
+            print string.join(s, "")
+        
+        for i in xrange(1, len(docSentenceNums)):
+            if docSentenceNums[i] - docSentenceNums[i - 1] != 2:
+                raise Exception("Must only contains couple sentences for each dialog.")
+        
+        dialogMatrixes = algorithm.transToTensor(dialogMatrixes, config.globalFloatType())
+        docSentenceNums = algorithm.transToTensor(docSentenceNums, numpy.int32)
+        sentenceWordNums = algorithm.transToTensor(sentenceWordNums, numpy.int32)
+        
+        n_batches = (len(docSentenceNums.get_value()) - 1) / batchSize + 1
+        isAStartSentenceNums = numpy.zeros(docSentenceNums.get_value()[-1] + 1, dtype=numpy.int32)
+        for i in docSentenceNums.get_value():
+            isAStartSentenceNums[i] = 1
+        isAStartSentenceNums = theano.shared(
+                 isAStartSentenceNums,
+                borrow=True
+            )
+        print "Train set size is ", len(docSentenceNums.get_value()) - 1
+        print "Batch size is ", batchSize
+        print "Number of training batches  is ", n_batches
+        print "Data loaded."
+        index = T.lscalar("index")
+        print "Compiling computing graph."
+#         from theano import ProfileMode
+#         profmode = ProfileMode(optimizer='fast_run', linker=theano.gof.OpWiseCLinker())
+        train_model = theano.function(
+             [index],
+             [e, errorSum, errorSumNegative, normalizationError],
+             updates=updates,
+             givens={
+                            self._corpusWithEmbeddings: dialogMatrixes,
+                            self._dialogSentenceCount: docSentenceNums[index * batchSize: (index + 1) * batchSize + 1],
+                            self._sentenceWordCount: sentenceWordNums,
+                            isAStartSentence: isAStartSentenceNums
+                        },
+            allow_input_downcast=True,
+#             mode=profmode
+         )
+        print "Compiled."
+        return train_model, n_batches
+    
+    def getTestFunction(self, param):
+        print "Compiling computing graph."
+        deploy_model = theano.function(
+             [self._corpusWithEmbeddings, self._dialogSentenceCount, self._sentenceWordCount],
+             [self._layer0.output, self._nextSentence, self._layer0.output],
+             allow_input_downcast=True
+         )
+        print "Compiled."
+        return deploy_model
+    
---
+++ algorithms/layers/sentenceEmbeddingMultiNN.py
@@ -0,0 +1,120 @@
+from theano import tensor as T, printing
+import theano
+import theano.tensor.signal.downsample as downsample
+# import theano.tensor.signal.conv as conv
+import theano.tensor.nnet.conv as conv
+import numpy
+import config
+
+class SentenceEmbeddingMultiNN:
+    
+    def __init__(self,
+                          corpus,
+                          docSentenceCount,
+                          sentenceWordCount,
+                          rng,
+                          wordEmbeddingDim,
+                          sentenceLayerNodesNum=[2, 2],
+                          sentenceLayerNodesSize=[(2, 2), (2, 1)],
+                          poolingSize=[(2, 1)],
+                          datatype=config.globalFloatType(),
+                          mode="average_exc_pad"):
+        self.__wordEmbeddingDim = wordEmbeddingDim
+        self.__sentenceLayerNodesNum = sentenceLayerNodesNum
+        self.__sentenceLayerNodesSize = sentenceLayerNodesSize
+        self.__poolingSize = list(poolingSize)
+        self.__WBound = 0.2
+        self.__MAXDIM = 10000
+        self.__datatype = datatype
+        
+        if len(poolingSize) != len(sentenceLayerNodesSize) - 1:
+            raise Exception("Please check the size of filter list and pooling size list.") 
+        
+        self.sentenceW = None
+        self.sentenceB = None
+        self.mode = mode
+        # Get sentence layer W
+        self.sentenceW = list()
+        
+        lastNodesNum = 1
+        for nodesNum, nodesSize in zip(sentenceLayerNodesNum, sentenceLayerNodesSize):
+            self.sentenceW.append(theano.shared(
+                numpy.asarray(
+                    rng.uniform(low=-self.__WBound, high=self.__WBound, size=(nodesNum, lastNodesNum, nodesSize[0], nodesSize[1])),
+                    dtype=datatype
+                ),
+                borrow=True
+                ))
+            lastNodesNum = nodesNum
+        
+        # Get sentence layer b
+        self.sentenceB = [theano.shared(value=numpy.zeros((nodesNum,), dtype=datatype), borrow=True) \
+                          for nodesNum in sentenceLayerNodesNum]
+        
+#         t = T.and_((self.shareRandge < docSentenceCount[-1] + 1),  (self.shareRandge >= docSentenceCount[0])).nonzero()
+        oneDocSentenceWordCount = sentenceWordCount[docSentenceCount[0]:docSentenceCount[-1] + 1]
+                
+#         p = printing.Print("corpus")
+#         corpus = p(corpus)
+
+        self.__poolingSize.append((self.__MAXDIM, 1))
+        sentenceResults, _ = theano.scan(fn=self.__dealWithSentence,
+                    non_sequences=[corpus],
+                     sequences=[dict(input=oneDocSentenceWordCount, taps=[-1, -0])])
+        
+        self.output = sentenceResults
+        self.params = self.sentenceW + self.sentenceB
+        self.outputDimension = self.__wordEmbeddingDim
+        
+#         for i in xrange(len(sentenceLayerNodesSize)):
+#             self.outputDimension = (self.outputDimension - sentenceLayerNodesSize[i][1] + 1) / self.__poolingSize[i][1] + 1
+        
+        for nodesSize, pSize in zip(sentenceLayerNodesSize, self.__poolingSize):
+            self.outputDimension = (self.outputDimension - nodesSize[1] + 1 -1) / pSize[1] + 1
+            
+        self.outputDimension *= sentenceLayerNodesNum[-1]
+    
+    def __dealWithSentence(self, sentenceWordCount0, sentenceWordCount1, docs):
+        
+#         p = printing.Print("sentenceWordCount0")
+#         sentenceWordCount0 = p(sentenceWordCount0)  
+#         p = printing.Print("sentenceWordCount1")
+#         sentenceWordCount1 = p(sentenceWordCount1)  
+#         t = T.and_((shareRandge < sentenceWordCount1),  (shareRandge >= sentenceWordCount0)).nonzero()
+        sentenceW = self.sentenceW
+        sentenceB = self.sentenceB
+        poolingSize = self.__poolingSize
+        sentence = docs[sentenceWordCount0:sentenceWordCount1]
+        
+        lastOutput = sentence.dimshuffle(['x', 'x', 0, 1])
+        
+#         p = printing.Print("lastOutput")
+#         lastOutput = p(lastOutput)
+        
+        count = 0
+        for w, b, poolsize in zip(sentenceW, sentenceB, poolingSize):
+            sentence_out = conv.conv2d(input=lastOutput, filters=w)
+            
+            count += 1
+#             p = printing.Print("sentence_out"+str(count))
+#             sentence_out = p(sentence_out)  
+            
+            sentence_pool = downsample.max_pool_2d(sentence_out, poolsize, mode=self.mode, ignore_border=False)
+            
+#             p = printing.Print("sentence_pool"+str(count))
+#             sentence_pool = p(sentence_pool)  
+            
+            bd = b.dimshuffle(['x', 0, 'x', 'x'])
+            
+#             p = printing.Print("bd"+str(count))
+#             bd= p(bd)  
+            
+            sentence_output = T.tanh(sentence_pool + bd)
+            lastOutput = sentence_output
+        
+        sentence_embedding = lastOutput.flatten(1)
+    
+#         p = printing.Print("sentence_embedding")
+#         sentence_embedding = p(sentence_embedding)
+        
+        return sentence_embedding
